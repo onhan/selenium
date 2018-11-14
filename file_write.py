2f82d9ee05d000592a110fe334a05ce72c520c0c
onhan <artem.shmahun@gmail.com>
2018-11-14 11:21:40 +0200

    site parsing and save something

---
+++ file_write.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.uk");
+
+file = open(r"C:\captures.html", "w")
+file.write("<!DOCTYPE html><html><head></head><body width=\"600px\">")
+
+file.write("<img src=\"data:image/png;base64,")
+file.write(driver.get_screenshot_as_base64())
+file.write("\">")
+
+file.write("</body></html>")
+file.close()
+
+driver.quit()
